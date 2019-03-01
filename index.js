3a6e1a06189173e001b4b6c59e686a2257e00461
Daniel <gustaw.daniel@gmail.com>
2019-03-01 23:18:00 +0100

    Events for Fetch and Message saved as const
    Changes similar to these for Imap and Notifier events.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Imap = require('imap'),
 const defaultDebug = debug('mailnotifier');
 
 /**
- * connection events from imap package, these strings should be used on imap listening
+ * Connection events from imap package, these strings should be used on imap listening
  *
  * source:
  * https://www.npmjs.com/package/imap
@@ -39,7 +39,34 @@ const CONNECTION_EVENT_END = 'end';
 // () - Emitted when the connection has ended.
 
 /**
- * this package events
+ * ImapMessage events from imap package (emitting object is called msgEmitter in imap package)
+ *
+ * source
+ * https://www.npmjs.com/package/imap
+ */
+const MESSAGE_EVENT_BODY = 'body';
+// (< ReadableStream >stream, < object >info) - Emitted for each requested body.
+const MESSAGE_EVENT_ATTRIBUTES = 'attributes';
+// (< object >attrs) - Emitted when all message attributes have been collected
+const MESSAGE_EVENT_END = 'end';
+// () - Emitted when all attributes and bodies have been parsed.
+
+/**
+ * ImapFetch events from imap package (emitting object is called bodyEmitter and is property of _curReq)
+ *
+ * source
+ * https://www.npmjs.com/package/imap
+ */
+const FETCH_EVENT_MESSAGE = 'message';
+// (< ImapMessage >msg, < integer >seqno) - Emitted for each message resulting from a fetch request.
+// seqno is the message's sequence number.
+const FETCH_EVENT_ERROR = 'error';
+// (< Error >err) - Emitted when an error occurred.
+const FETCH_EVENT_END = 'end';
+// () - Emitted when all messages have been parsed.
+
+/**
+ * This package events
  *
  * source:
  * README.md
@@ -55,7 +82,7 @@ const NOTIFIER_EVENT_END = 'end';
 
 class Notifier extends EventEmitter {
 
-    constructor(opts, dbg) {
+    constructor(opts, dbg) { // this not creates new connection, only configure notifier
         super();
         const self = this;
         self.dbg = defaultDebug;
@@ -71,9 +98,11 @@ class Notifier extends EventEmitter {
         }
     }
 
-    start() {
+    start() { // this is real entry point of program
         const self = this;
 
+        // we creating queue that will call scan mailbox, in practice task is only for debug and callback too
+        // to configure scanning you should use option `search` in configuration object forwarded to constructor
         // q - queue object - https://caolan.github.io/async/docs.html#QueueObject
         const q = async.queue(function(task, callback) { // docs: https://caolan.github.io/async/docs.html#queue
             self.dbg('process queue ' + task.name);
@@ -85,20 +114,21 @@ class Notifier extends EventEmitter {
             self.dbg('all items have been processed');
         };
 
+        // we configuring connection
         self.imap = new Imap(self.options);
-        self.imap.once(CONNECTION_EVENT_END, function () {
+        self.imap.once(CONNECTION_EVENT_END, function () { // all end of connection should be intercepted by notifier
             self.dbg('imap end');
             self.emit(NOTIFIER_EVENT_END);
         });
-        self.imap.once(CONNECTION_EVENT_ERROR, function (err) {
+        self.imap.once(CONNECTION_EVENT_ERROR, function (err) { // similarly error of connection
             self.dbg('imap error : %s', err);
             self.emit(NOTIFIER_EVENT_ERROR, err);
         });
-        self.imap.once(CONNECTION_EVENT_CLOSE, function (hasError) {
+        self.imap.once(CONNECTION_EVENT_CLOSE, function (hasError) { // potentially bugged connection breaks
             self.dbg('imap close : %s', hasError ? 'errored ' + hasError : 'normal');
-            self.emit(NOTIFIER_EVENT_END);
+            self.emit(NOTIFIER_EVENT_END); // are treated as end for notifier, it potentially allow to reconnect
         });
-        self.imap.on(CONNECTION_EVENT_UIDVALIDITY, function (uidValidity) {
+        self.imap.on(CONNECTION_EVENT_UIDVALIDITY, function (uidValidity) { // uidvalidity is captured but nothing happens there
             self.dbg('new uidvalidity : %s', uidValidity);
         });
         self.imap.once(CONNECTION_EVENT_READY, function () {
@@ -145,29 +175,29 @@ class Notifier extends EventEmitter {
                 markSeen: self.options.markSeen !== false,
                 bodies: ''
             });
-            fetch.on('message', function (msg) {
+            fetch.on(FETCH_EVENT_MESSAGE, function (msg) {
                 let uid, flags;
-                msg.on('attributes', function(attrs) {
+                msg.on(MESSAGE_EVENT_ATTRIBUTES, function(attrs) {
                     uid = attrs.uid;
                     flags = attrs.flags;
                     self.dbg("Message uid", attrs.uid);
                 });
                 const mp = new MailParser();
-                mp.once('end', function (mail) {
+                mp.once(MESSAGE_EVENT_END, function (mail) { // this is technically event of mail parser, not message
                     mail.uid = uid;
                     mail.flags = flags;
                     self.emit(NOTIFIER_EVENT_MAIL, mail);
                     self.dbg('found mail '+mail.headers["message-id"]);
                 });
-                msg.once('body', function (stream, info) {
+                msg.once(MESSAGE_EVENT_BODY, function (stream, info) {
                     stream.pipe(mp);
                 });
             });
-            fetch.once('end', function () {
+            fetch.once(FETCH_EVENT_END, function () {
                 self.dbg('Done fetching all messages!');
                 callback();
             });
-            fetch.once('error', function (err) {
+            fetch.once(FETCH_EVENT_ERROR, function (err) {
                 self.dbg('fetch error : ', err);
                 self.emit(NOTIFIER_EVENT_ERROR, err);
                 callback();
